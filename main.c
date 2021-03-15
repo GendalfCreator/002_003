2d8752d706498e5e2f4ac3bee5fe5bbee7829c28
Alex <frstfng@gmail.com>
2021-03-15 15:44:54 +0300

    recursive dec to bin

--- main.c
+++ main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
+void dec_to_bin_recursive(int dec) {
+  if (dec / 2 > 0) {
+      dec_to_bin_recursive(dec / 2);
+    }
+  printf("%d", dec % 2);
+}
+
+void rec(int a) {
+
+  if (a > 0) {
+      rec(a - 1);
+    }
+  printf("%3d", a);
+
+}
+
 int main()
 {
-  printf("Hello World!\n");
+  dec_to_bin_recursive(6);
+//  rec(10);
+  printf("\n");
   return 0;
 }
