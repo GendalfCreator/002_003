d5a133924d7f831c3297cc45caa9d39a7ed39a2d
Alex <frstfng@gmail.com>
2021-03-16 23:22:32 +0300

    4 quests completed

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+//01. Рекурсивная функиця перевода десятичного числа в двоичное
 void dec_to_bin_recursive(int dec) {
   if (dec / 2 > 0) {
       dec_to_bin_recursive(dec / 2);
@@ -7,6 +8,7 @@ void dec_to_bin_recursive(int dec) {
   printf("%d", dec % 2);
 }
 
+//02. Итерационная функция возведения числа в степень
 int power(int a, int b) {
   int result = 1;
 
@@ -17,6 +19,7 @@ int power(int a, int b) {
   return result;
 }
 
+//03. Рекурсивная функция возведения числа в степень
 int power_recoursive(int a, int b) {
   if (b == 0) {
       return 1;
@@ -29,10 +32,78 @@ int power_recoursive(int a, int b) {
   return a;
 }
 
+//04. Рекурсивная функция возведения числа в степень с использованием свойства чётности
+int power_even(int a, int b) {
+  if (b / 2 != 0) {
+      if (b % 2 == 0) {
+          a = power_even(a * a, b / 2);
+        }
+      else {
+          a = a * power_even(a, b - 1);
+        }
+    }
+
+  return a;
+}
+
+//05. Функция расчёта количества путей короля с препятствиями
+enum checker {EMPTY = '_', WALL = '#'};
+
+char field[5][5];
+
+void init_field(){
+  for (int y = 0; y < 5; y++) {
+      for (int x = 0; x < 5; x++) {
+          field[x][y] = EMPTY;
+        }
+    }
+}
+
+void print_field(){
+  for (int y = 0; y < 5; y++) {
+      for (int x = 0; x < 5; x++) {
+          printf("%4c", field[x][y]);
+        }
+      printf("\n");
+    }
+  printf("\n");
+
+}
+
+int king_way(int x, int y) {
+  if (x == 0 && y == 0) {
+      return 0;
+    }
+  else if (x == 0 || y == 0) {
+      return 1;
+    }
+  else {
+      if (field[x - 1][y] == WALL) {//Если клетка слева занята
+          return king_way(x, y -1);//Обойти сверху
+        }
+        else if (field[x][y - 1] == WALL) {//Если клетка сверху занята
+          return king_way(x -1, y);//Обойти слева
+        }
+      else {//Иначе идти по обоим направлениям
+          return  king_way(x, y -1) + king_way(x -1, y);
+        }
+    }
+}
+
 int main()
 {
-  dec_to_bin_recursive(6);
+  init_field();
 
-  printf("\n%3d\n", power_recoursive(2, 8));
+  field[3][3] = WALL;
+
+  print_field();
+
+  for (int y = 0; y < 5; y++) {
+      for (int x = 0; x < 5; x++) {
+          printf("%4d", king_way(x, y));
+        }
+      printf("\n");
+    }
+
   return 0;
 }
